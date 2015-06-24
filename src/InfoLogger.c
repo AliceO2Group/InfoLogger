1fd39820e14ba604fa858db64e1caa0973bd56b6
sy-c <o2.syc@cern.ch>
2015-06-24 11:54:31 +0200

    cosmetics

--- src/InfoLogger.c
+++ src/InfoLogger.c
@@ -4,45 +4,51 @@
 #include <stdarg.h>
 #include <time.h>
 
-typedef struct {  
+typedef struct
+{
 } InfoLoggerHandleData;
 
-
-int infoLoggerOpen(InfoLoggerHandle *handle) {
-  if (handle==NULL) { return __LINE__; }
+int infoLoggerOpen(InfoLoggerHandle *handle)
+{
+  if (handle == NULL) {
+    return __LINE__;
+  }
   return 0;
 }
 
-int infoLoggerClose(InfoLoggerHandle handle) {
- InfoLoggerHandleData *handleData;
- handleData=(InfoLoggerHandleData *)handle;
- if (handleData==NULL) { return __LINE__; }
- return 0;
+int infoLoggerClose(InfoLoggerHandle handle)
+{
+  InfoLoggerHandleData *handleData;
+  handleData = (InfoLoggerHandleData *) handle;
+  if (handleData == NULL) {
+    return __LINE__;
+  }
+  return 0;
 }
 
-int infoLoggerLog(InfoLoggerHandle handle, const char *message, ...) {
- InfoLoggerHandleData *handleData;
- handleData=(InfoLoggerHandleData *)handle;
- if (handleData==NULL) { return __LINE__; }
+int infoLoggerLog(InfoLoggerHandle handle, const char *message, ...)
+{
+  InfoLoggerHandleData *handleData;
+  handleData = (InfoLoggerHandleData *) handle;
+  if (handleData == NULL) {
+    return __LINE__;
+  }
 
+  char buffer[1024];
+  int len;
 
- char buffer[1024];
- int len;
- 
- time_t      now;
- struct tm   tm_str;
- now = time(NULL);
- localtime_r(&now,&tm_str);
- len=strftime(buffer, sizeof(buffer), "%Y-%m-%d %T\t", &tm_str);
+  time_t now;
+  struct tm tm_str;
+  now = time(NULL);
+  localtime_r(&now, &tm_str);
+  len = strftime(buffer, sizeof(buffer), "%Y-%m-%d %T\t", &tm_str);
 
+  va_list ap;
+  va_start(ap, message);
+  vsnprintf(&buffer[len], sizeof(buffer) - len, message, ap);
+  va_end(ap);
 
+  printf("%s\n", buffer);
 
- va_list ap;
- va_start(ap,message);
- vsnprintf(&buffer[len],sizeof(buffer)-len,message,ap);
- va_end(ap);
-
-  printf("%s\n",buffer);
- 
- return 0;
+  return 0;
 }

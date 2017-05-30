5825161c4ea1f5ffbafbd948bd0b12e7091229c6
Sylvain Chapeland <o2.syc@cern.ch>
2017-05-30 16:31:46 +0200

    file with some global defaults

---
+++ src/infoLoggerDefaults.h
@@ -0,0 +1,7 @@
+#define INFOLOGGER_DEFAULT_CONFIG_PATH "/etc/infoLogger.cfg"
+
+// name of the section where to find parameters
+#define INFOLOGGER_CONFIG_SECTION_NAME_ADMIN "admin"
+
+// Infologger DB: name of the main message table
+#define INFOLOGGER_TABLE_MESSAGES "messages"

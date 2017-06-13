bee3c438d16554e01977a24a1441e9f62bc3c088
Sylvain Chapeland <o2.syc@cern.ch>
2017-06-13 13:43:08 +0200

    added new variables

--- src/infoLoggerDefaults.h
+++ src/infoLoggerDefaults.h
@@ -2,6 +2,20 @@
 
 // name of the section where to find parameters
 #define INFOLOGGER_CONFIG_SECTION_NAME_ADMIN "admin"
+#define INFOLOGGER_CONFIG_SECTION_NAME_SERVER "infoLoggerServer"
+#define INFOLOGGER_CONFIG_SECTION_NAME_INFOLOGGERD "infoLoggerD"
+
 
 // Infologger DB: name of the main message table
 #define INFOLOGGER_TABLE_MESSAGES "messages"
+
+// default infoLoggerServer listening port for infoLoggerD
+#define INFOLOGGER_DEFAULT_SERVER_RX_PORT 6006
+
+// default infoLoggerServer listening port for infoBrowser
+#define INFOLOGGER_DEFAULT_SERVER_TX_PORT 6102
+
+
+// default listening socket name for infoLoggerD
+#define INFOLOGGER_DEFAULT_LOCAL_SOCKET "infoLoggerD"
+

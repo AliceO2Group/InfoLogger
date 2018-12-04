13065683c71ed5b3538b2e03103b0d20e746b02b
Sylvain Chapeland <o2.syc@cern.ch>
2018-12-04 15:02:20 +0100

    replacement for InfoLoggerConfig.cmake.in, as no substitutions were done

--- cmake/InfoLoggerConfig.cmake
+++ cmake/InfoLoggerConfig.cmake
@@ -1,1 +1,17 @@
-include("${CMAKE_CURRENT_LIST_DIR}/InfoLoggerTargets.cmake")
+# get path to this file
+get_filename_component(INFOLOGGER_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+message(STATUS "InfoLogger found - using cmake targets in ${INFOLOGGER_CMAKE_DIR}")
+
+# dependencies for clients using the library
+# we now package a standalone version of libInfoLogger
+# so the lines below are not needed - kept for reference
+# include(CMakeFindDependencyMacro)
+# find_dependency(Common REQUIRED)
+
+# declare InfoLogger target
+if(NOT TARGET AliceO2::InfoLogger)
+  include("${INFOLOGGER_CMAKE_DIR}/InfoLoggerTargets.cmake")
+endif()
+
+# declare variable for backward compatibility in client programs not using cmake targets
+set(InfoLogger_LIBRARIES AliceO2::InfoLogger)

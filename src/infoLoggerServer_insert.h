4c808f682ee9cbd33e633100c2ba93711a50648d
Sylvain Chapeland <o2.syc@cern.ch>
2019-08-05 15:46:06 +0200

    added copyright header

--- src/infoLoggerServer_insert.h
+++ src/infoLoggerServer_insert.h
@@ -1,3 +1,13 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
 /* Log message interface */
 /* 07 Apr 2005 - Added run number to fields */
 /* 02 Dec 2005 - Added system to fields */
--- src/transport_cache.h
+++ src/transport_cache.h
@@ -1,3 +1,13 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
 /**
  * This file defines an interface to a FIFO cache. The FIFO is filled with files
  * to transport, and in normal mode is just a memory cache. But if it is filled

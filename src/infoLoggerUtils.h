5334cffece013274f86957253c9e6de7bbfda07f
Sylvain Chapeland <o2.syc@cern.ch>
2021-07-23 12:11:54 +0200

    updated o2 copyright

--- src/infoLoggerUtils.h
+++ src/infoLoggerUtils.h
@@ -1,8 +1,9 @@
-// Copyright CERN and copyright holders of ALICE O2. This software is
-// distributed under the terms of the GNU General Public License v3 (GPL
-// Version 3), copied verbatim in the file "COPYING".
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
 //
-// See http://alice-o2.web.cern.ch/license for full licensing information.
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
 //
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
@@ -13,3 +14,4 @@
 
 // convert a string comma-separated list of key=value pairs to a map
 int getKeyValuePairsFromString(const std::string& input, std::map<std::string, std::string>& output);
+
